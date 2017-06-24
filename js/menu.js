78a2785acc6414d1db10ad7e038b9d4287008c35
danilkrivorotov <danilka1608@mail.ru>
2017-06-24 14:39:42 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,7 @@
-/* Открытие меню */
 var main = function() {
 	$('.head-nav-bar').click(function() {
 		$('.menu').animate({ top: '0px'}, 200);
-	})
-}
-/* Закрытие меню */
+	})}
 	$('.close-menu').click(function() {
 		$('.menu').animate({ top: '-260px'}, 200);
 	})
