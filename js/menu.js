6176d565342604382c6bcea11a6540ac3fe0b7af
danilkrivorotov <danilka1608@mail.ru>
2017-08-02 13:06:52 +0300

    Delete menu.js

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-var main = function() {
-	$('.head-nav-bar').click(function() {
-		$('.menu').animate({ top: '0px'}, 200);
-	})}
-	$('.close-menu').click(function() {
-		$('.menu').animate({ top: '-260px'}, 200);
-	})
-$(document).ready(main);
